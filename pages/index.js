ef7a6b25dc2dbfb5cad512e283100c9a5d4c0036
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-03-16 21:05:38 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import { Calendar } from "lucide-react";
+import SignUpModal from "../components/auth/signup";
+
+
 const HomePage = () => {
   const [location, setLocation] = useState("");
   const [startDate, setStartDate] = useState("");
